9f1c5d27f53355319bab9d08a50e9ecef854def9
boxxy <91877601+devbo-xy@users.noreply.github.com>
2022-06-24 11:10:10 +0700

    update

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,2 @@
 export { default as HeroMed } from "./gifHome.gif";
 export { default as GifDoctor } from "./gifDoctor.gif";
-export { default as GifTes } from "./gifTesCovid.gif";
-export { default as GifDiabetes } from "./gifDiabetes.gif";
-export { default as GifJantung } from "./gifJantung.gif";
-export { default as StrukturOrg } from "./strukturOrg.png";
-export { default as GrupLine } from "./grupLine.svg";
